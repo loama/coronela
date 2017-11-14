e59792c5a1c649014dfa95b755aad8b713c5c338
Jaime <loama18@gmail.com>
2017-11-13 23:38:59 -0600

    Add language changer

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,31 +4,42 @@
     <div class="header">
       <div class="texture"></div>
       <div class="logo"></div>
+      <div class="lang">
+        <span v-bind:class="{active: language === 'spanish'}" v-on:click="language='spanish'"> español &nbsp |</span>
+        <span v-bind:class="{active: language === 'english'}" v-on:click="language='english'"> english </span>
+      </div>
+
     </div>
 
     <ul class="navbar" v-bind:class="{ scrolled: navbarBackground}">
       <li :class="{active:scrollPos == 0}">
-        <a @click="$scrollTo(0)">reservaciones</a>
+        <a v-if="this.language === 'spanish'" @click="$scrollTo(0)">reservaciones</a>
+        <a v-if="this.language === 'english'" @click="$scrollTo(0)">reservations</a>
       </li>
       |
       <li :class="{active:scrollPos == 1}">
-        <a @click="$scrollTo(1)">menús</a>
+        <a v-if="this.language === 'spanish'" @click="$scrollTo(1)">menús</a>
+        <a v-if="this.language === 'english'" @click="$scrollTo(1)">menus</a>
       </li>
       |
       <li :class="{active:scrollPos == 2}">
-        <a @click="$scrollTo(2)">ubicación</a>
+        <a v-if="this.language === 'spanish'" @click="$scrollTo(2)">ubicación</a>
+        <a v-if="this.language === 'english'" @click="$scrollTo(2)">location</a>
       </li>
       |
       <li :class="{active:scrollPos == 3}">
-        <a @click="$scrollTo(3)">prensa</a>
+        <a v-if="this.language === 'spanish'" @click="$scrollTo(3)">prensa</a>
+        <a v-if="this.language === 'english'" @click="$scrollTo(3)">press</a>
       </li>
       |
       <li :class="{active:scrollPos == 4}">
-        <a @click="$scrollTo(4)">eventos</a>
+        <a v-if="this.language === 'spanish'" @click="$scrollTo(4)">eventos</a>
+        <a v-if="this.language === 'english'" @click="$scrollTo(4)">events</a>
       </li>
       |
       <li :class="{active:scrollPos == 5}">
-        <a @click="$scrollTo(5)">contacto</a>
+        <a v-if="this.language === 'spanish'" @click="$scrollTo(5)">contacto</a>
+        <a v-if="this.language === 'english'" @click="$scrollTo(5)">contact</a>
       </li>
     </ul>
 
@@ -44,16 +55,53 @@
       </slide>
     </carousel>
 
+    <div class="watermark"></div>
+
     <div v-scroll-spy="scrollPos">
-      <div class="section reservations" style="height:1000px">
-        <h1>· &nbsp RESERVACIONES &nbsp ·</h1>
+
+      <div class="section reservations">
+        <h1 v-if="this.language === 'spanish'">· &nbsp Reservaciones &nbsp ·</h1>
+        <h1 v-if="this.language === 'english'">· &nbsp Reservations &nbsp ·</h1>
         <p class="detail">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros.</p>
-        <div class="button"> reserva aquí </div>
+        <div v-if="this.language === 'spanish'" class="button"> reserva aquí </div>
+        <div v-if="this.language === 'english'" class="button"> reserve here </div>
+        <hr>
       </div>
-      <div class="section" style="height:1200px">
-        <h1>Header 2</h1>
+
+      <div class="section menus">
+        <h1 v-if="this.language === 'spanish'">· &nbsp Menús &nbsp ·</h1>
+        <h1 v-if="this.language === 'english'">· &nbsp Menus &nbsp ·</h1>
+        <p v-if="this.language === 'spanish'">Contenido</p>
+        <p v-if="this.language === 'english'">Content</p>
+        <hr>
+      </div>
+
+      <div class="section location">
+        <h1 v-if="this.language === 'spanish'">· &nbsp Ubicación &nbsp ·</h1>
+        <h1 v-if="this.language === 'english'">· &nbsp Location &nbsp ·</h1>
+        <p v-if="this.language === 'spanish'">Contenido</p>
+        <p v-if="this.language === 'english'">content</p>
+        <hr>
+      </div>
+
+      <div class="section press">
+        <h1>· &nbsp RESERVACIONES &nbsp ·</h1>
         <p>Content</p>
+        <hr>
       </div>
+
+      <div class="section events">
+        <h1>· &nbsp RESERVACIONES &nbsp ·</h1>
+        <p>Content</p>
+        <hr>
+      </div>
+
+      <div class="section contact">
+        <h1>· &nbsp RESERVACIONES &nbsp ·</h1>
+        <p>Content</p>
+        <hr>
+      </div>
+
     </div>
 
   </div>
@@ -66,6 +114,7 @@
     name: 'Home',
     data () {
       return {
+        language: 'spanish',
         navbarBackground: false,
         scrollPos: 0
       }
@@ -98,6 +147,7 @@
     box-shadow: 0 -1px 4px #888;
     background: #FAFAFA;
     border-radius: 8px;
+    position: relative;
   }
 
   .header .texture {
@@ -114,6 +164,31 @@
     position: absolute;
     top: 32px;
     left: calc(50% - 134px);
+  }
+
+  .lang {
+    position: absolute;
+    top: 80px;
+    right: -10px;
+    transform: rotate(270deg);
+    z-index: 2000;
+    float: center;
+    display: inline;
+    font-size: 14px;
+  }
+
+  .lang span {
+    margin-right: 8px;
+    cursor: pointer;
+  }
+
+  .lang span.active {
+    font-family: 'Brandon-Bold';
+  }
+
+  .VueCarousel {
+    position: relative;
+    z-index: 400;
   }
 
   .carousel-img {
@@ -188,6 +263,22 @@
   .reservations .button:hover {
     color: white;
     background: #1c449c;
+  }
+
+  .section hr {
+    width: 608px;
+    max-width: calc(100% - 236px);
+    margin: 0 auto;
+  }
+
+  .watermark {
+    position: fixed;
+    top: 300px;
+    right: calc(50% - 640px);
+    width: 168px;
+    height: 264px;
+    background-image: url('../assets/sello/sello_coronela.png');
+    z-index: 200;
   }
 
 </style>
